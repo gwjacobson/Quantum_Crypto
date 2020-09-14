f2725e9a94b92964681ff947346f152c31ee92f2
gwjacobson <graemejacobson@gmail.com>
2020-09-14 00:45:27 -0500

    fixed RSA, 99% working

--- RSA.py
+++ RSA.py
@@ -1,32 +1,11 @@
 import random
 import sympy
+import math
+from math import gcd
 
-
-#create the public and private keys. Public key can be sent to anyone
-#wanting to send a message or seen by others. The private key is used to
-#decrypt the message. Returns a private key.
-def KeyDef(n, phi, e):
-	k = 0 #value to make private key an int
-	for i in range(1, 100): #finding value of k
-		if ((i*phi+1)/e) % 1 == 0:
-			k = i
-			break
-		else:
-			continue
-	return ((k*phi+1)/e) #private key
-
-#Encrypt a inputted message. The message comes in as an int and will be made into
-#an encrypted using n and e. It will return the encrypted message, c.
-def Encrypt(m, n, e):
-	return (m**e) % n
-
-#Decrypt a message, c, using d and n. This will return a decrypted
-#message.
-def Decrypt(c, d, n):
-	return (c**d) % n
 
 #variables for public and private keys
-primes = list(sympy.primerange(0,1000000))
+primes = list(sympy.primerange(0,50))
 
 p1 = random.choice(primes) #first prime
 p2 = random.choice(primes) #second prime
@@ -34,33 +13,28 @@ p2 = random.choice(primes) #second prime
 n = p1*p2 #large prime for public key
 
 phi = (p1 - 1)*(p2 - 1) #Euler's totient
+print('phi: '+str(phi)+', n: '+str(n)+', p1: '+str(p1)+', p2:'+str(p2))
 
-e = 0 #exponent for encrypt and decrypt
-for i in range(1, 100): #smallest number where phi/e and phi don't share factor
-	a = phi
-	b = phi/i
-	if i % 2 != 0:
-		if a % i == b % i == 0:
-			continue
-		else:
-			e = i
-			break
-	else:
-		continue
+#find the exponent for the private key
+#e is a coprime of phi
+for e in range(2, phi):
+	if gcd(phi, e) == 1:
+		break
 
-d = KeyDef(n, phi, e) #find private key
+#this is the private key
+for d in range(2,1000):
+	if (d*e) % phi == 1:
+		break
 
 print('Your private key is: '+str(d))
 print('Your public key is: '+str(n)+', '+str(e))
 
 message = input('Enter your message: ') #user inputs message to encrypt
-print('Your message is: '+message)
-
 mess_int = int(message) #change message into int
+print('Your message is: '+str(mess_int))
 
-c = Encrypt(mess_int, n, e) #encrypted message
+c = pow(mess_int, e, n) #encrypted message
 print('The encrypted message is: '+str(c))
 
-m = Decrypt(c, d, n) #the decrypted message
-m = int(round(m))
+m = pow(c, d, n) #the decrypted message
 print('The decrypted message is: '+str(m))
