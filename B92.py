595f40d91e56f1825bacab683755367e702a3a4f
gwjacobson <graemejacobson@gmail.com>
2020-11-18 17:03:14 -0600

    b92 alg creating key

--- B92.py
+++ B92.py
@@ -5,6 +5,8 @@ import string
 
 #implementation of B92 QKD protocol
 
+sim = Aer.get_backend('qasm_simulator') #local backend to run simulation
+
 bits = [0,1] #classical bits alice can choose
 
 a = [] #initialize alices bit string
@@ -14,15 +16,16 @@ for i in range(0,8): #choose random bit string
 
 print("Alice's String: "+str(a))
 
-qc = QuantumCircuit(8) #create 8 quibit circuit for 8 bits
+qr = QuantumRegister(8)
+cr = ClassicalRegister(8)
+qc = QuantumCircuit(qr, cr)#create 8 quibit circuit for 8 bits
 for qubit in range(8): #apply basis on each qubit
     if a[qubit] == 1: #diagonal basis on 1 bit
         qc.h(qubit)
     else: #computational basis on 0 bit
         continue
 
-#for qubit in range(8): #separate alice and bob basis
-    #qc.barrier(qubit)
+qc.barrier()
 
 
 a_prime = [] #initial Bobs bit string
@@ -38,4 +41,27 @@ for qubit in range(8): #apply basis on each qubit for bob
     else: #computational basis on 1 bit
         continue
 
+qc.barrier()
+
+qc.measure(qr, cr) #measure all the qubits
+
+job = execute(qc, sim, shots=1, memory=True) #get the results of each meausurment
+ket = job.result().get_memory()
+
 print(qc)
+
+print(ket) #our measurement results
+ket1 = ket[0] #get the string of bit measurements
+m = list(map(lambda i:i, ket1)) #split the string of measurements
+m_bits = list(map(int, m)) #turn each bit into an int
+m_bits.reverse() #for correct order in alice bits
+print(m_bits)
+
+key = [] #our quantum key
+for b in range(len(m_bits)):
+    if m_bits[b] == 1: #if we measured a 1, use in key
+        key.append(a[b]) #use the bit from alice's bit string
+    else:
+        continue
+
+print('Quantum Key: '+str(key))
